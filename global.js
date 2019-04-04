ce99104764b7668931e05142c90d50f14535e678
Gabe Wright <gabe1331@gmail.com>
2019-04-04 10:54:15 -0400

    first commit

---
+++ global.js
@@ -0,0 +1,42 @@
+
+// Welcome! Here's an example of a console logged response
+console.log(4 + 5)
+
+/***********************/
+/* Part 1: Expressions */
+/***********************/
+
+//1.) Log an expression that evaluates to the sum of two integers
+// your code here 
+console.log('answer 1')
+
+// 2.) log an expression that evaluates to the difference of two integers
+// your code here 
+console.log('answer 2')
+
+// 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
+// your code here 
+console.log('answer 3')
+
+// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// your code here 
+console.log('answer 4')
+
+/**********************/
+/* Part 2: Statements */
+/**********************/
+
+// 5.) Assign a variable and console log the declaration
+// your code here 
+console.log('answer 5')
+
+// 6.) Console log a variable assigned to the sum of two numbers
+// your code here
+console.log('answer 6')
+
+// 7.) Uncomment this if else statement and add a logical expression as the condition
+//if (/*your code here*/) {
+//    console.log('It was!')
+//} else {
+//    console.log('It was not..')
+//}
